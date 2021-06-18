967a96edbb61acc1a0d4bbce90360a16aba457ba
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import 'svelte';
-import PolarityIcon from './PolarityIcon.svelte';
-export { PolarityIcon };
-export * from './polarity_class_';
-export * from './polarity_text_';
-//# sourceMappingURL=src/index.js.map
